83516a2490e65d0b7c5ac8bd07661cef74856576
Alin <lupascu.marius@gmail.com>
2024-03-12 12:05:14 -0600

    update appinfo title

--- appInfo.js
+++ appInfo.js
@@ -8,9 +8,8 @@ document.addEventListener('DOMContentLoaded', function() {
         const app = apps.find(app => app.id.toString() === appId); // Find the app by ID
         if (app) {
           // Update the page with the app's details
+          document.title = app.title + " - Alin Lupascu"
           document.getElementById('appImage').src = app.image;
-        //   document.getElementById('screenshot1').src = app.screenshot1;
-        //   document.getElementById('screenshot2').src = app.screenshot2;
           document.getElementById('appTitle').textContent = app.title;
           document.getElementById('appDescription').textContent = app.description;
           document.getElementById('download_url').href = app.download_url;
